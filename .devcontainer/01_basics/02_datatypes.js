f735f301f056f710a22ebe5b7851ff799eabb003
pratiksachinjadhav <115269416+pratiksachinjadhav@users.noreply.github.com>
2024-02-04 11:53:31 +0000

    datatypes discussed

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+//number==>2 to power 53
+//number
+//boolean
+//string
+//bigint
+//null
+//undefined
+//symbol==>unique
+
+let age=20
+let a= null
+console.log(typeof null);   //  O/p object
+console.log(typeof undefined);   //   O/p undefined
+console.log(typeof age);
